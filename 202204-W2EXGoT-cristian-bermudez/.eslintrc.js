3acc118b154f0f6737b849ad89e81bdb1d2aefd8
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-15 15:48:41 +0200

    initial commit

---
+++ 202204-W2EXGoT-cristian-bermudez/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
